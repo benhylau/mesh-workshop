76ca0f11f768bcdc22c4afebef23cd7e35a0402e
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2018-03-14 20:43:47 -0400

    [[ needs bash not sh

--- src/sh/02-raspberrypi2.sh
+++ src/sh/02-raspberrypi2.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 if [[ "$(cat /sys/firmware/devicetree/base/model | awk '{print $3}')" == "2" ]]; then
     sed -i "s/ath9k_htc //" /etc/systemd/network/85-wlan-mesh.link
     sed -i "s/ath9k_htc //" /etc/systemd/network/85-wlan-mesh.network
